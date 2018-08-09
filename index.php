c77ba47041c7d8a296c3187f5875b2af1c9fb515
Jody <jlerch@sortimo.de>
2018-08-09 11:45:11 +0200

    write log function implemented

--- index.php
+++ index.php
@@ -4,5 +4,9 @@ error_reporting(E_ALL | E_WARNING | E_NOTICE);
 ini_set('display_errors', TRUE);
 
 include 'functions/print_ob.php';
+include 'functions/write_log.php';
 
-print_ob("foobar");
+print_ob($_SERVER);
+print_ob($_GET);
+
+// write_log("info", "testomate");
